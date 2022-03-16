95aca3d15f67d41583f4aef1b40a913c1ba586ed
Alexander Levterov <shuroffrashiday@yandex.ru>
2022-03-16 11:42:44 +0300

    begin design development

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import { Article, Brand, CTA, Navbar, Feature } from './components'
 
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  WhatGPT3,
+  Header,
+} from './containers'
+import './App.css'
 const App = () => {
   return (
-    <div>
-      <h1>GPT-3</h1>
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   )
 }
